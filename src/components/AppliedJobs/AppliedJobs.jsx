28bb45446eb9da1a8c3d768f1975abec9e1bf0df
Programming Hero <programming.hero1@gmail.com>
2023-09-20 23:41:17 -0500

    save applied jobs to local storage

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../../utility/localstorage";
 
 
 const AppliedJobs = () => {
-    
+    const jobs = useLoaderData();
+    useEffect( () =>{
+        const storedJobIds = getStoredJobApplication();
+        if(jobs.length > 0){
+            const jobsApplied = jobs.filter(job => ) 
+        }
+    }, [])
     return (
         <div>
             <h2>Jobs I applied</h2>
